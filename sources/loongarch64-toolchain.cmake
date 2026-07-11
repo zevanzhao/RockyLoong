7de5208f04bf767b2f412580ee12e8f17e57dbcd
zevanzhao <yafanzhao@163.com>
2026-07-11 23:07:42 +0800

    fix: 继续修改文档，进行到tcl软件的rpm包编译

---
+++ sources/loongarch64-toolchain.cmake
@@ -0,0 +1,5 @@
+# loongarch64-toolchain.cmake
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR loongarch64)
+set(CMAKE_C_COMPILER loongarch64-unknown-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER loongarch64-unknown-linux-gnu-g++)
